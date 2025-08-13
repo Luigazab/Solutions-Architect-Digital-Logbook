5202f249b6695bf2dc0fc67bd645ca91234d07b5
Luisha Gail Zaballero <155989934+Luigazab@users.noreply.github.com>
2025-08-13 13:37:17 +0800

    Add Customer page and route to application
    Introduces a new Customer page component and adds its route to the main router. Also makes a minor adjustment to the customer modal HTML by removing the 'flex' class from the modal container.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import DashboardLayout from './layouts/DashboardLayout.jsx';
 import Home from './pages/Home';
 import LogActivity from './pages/LogActivity';
+import Customer from './pages/Customer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Home />} /> {/* index = default content */}
           <Route path="log-activity" element={<LogActivity />} />
+          <Route path="customer" element={<Customer />} />
         </Route>
       </Routes>
     </BrowserRouter>
