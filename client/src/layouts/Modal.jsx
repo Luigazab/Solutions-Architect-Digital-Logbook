7b7fb0a85db9420e738428ddf4cc954cf2218193
Luisha Gail Zaballero <155989934+Luigazab@users.noreply.github.com>
2025-08-15 17:29:20 +0800

    Update Modal.jsx

--- client/src/layouts/Modal.jsx
+++ client/src/layouts/Modal.jsx
@@ -3,7 +3,7 @@ export default function Modal({ isOpen, onClose, title, children }) {
 
   return (
     <div style={{ backgroundColor: 'rgba(0,0,0,0.5)' }} className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-lg w-full max-w-xl p-6 relative">
+      <div className="m-4 bg-white rounded-lg shadow-lg w-full max-w-xl p-6 relative">
         <button onClick={onClose} className="absolute top-2 right-6 text-4xl text-gray-500 hover:text-gray-700">&times;</button>
         <h2 className="text-xl font-semibold mb-4">{title}</h2>
         {children}
