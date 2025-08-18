415c614392bf6b1b4995e69613b607a3341e9141
Luisha Gail Zaballero <155989934+Luigazab@users.noreply.github.com>
2025-08-18 17:10:30 +0800

    Change Login UI
    Login now have same footer and header as the other pages

--- client/src/layouts/AuthenticationLayout.jsx
+++ client/src/layouts/AuthenticationLayout.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function AuthenticationLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 text-gray-800">
-      <header className="bg-white shadow p-4">
-        <h1 className="text-xl font-bold">SolArch login page</h1>
-      </header>
-      <main className="p-6 space-y-4 flex-grow">
+      <Navbar></Navbar>
+      <main className="flex-grow flex items-center justify-center">
         {children || <Outlet />}
       </main>
-      <footer className="bg-white shadow p-4 text-center">
-        <p>&copy; AWOOOO</p>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 }
