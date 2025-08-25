30dbf7f48a9f4a4dd7b0c1882d7776cdd261c4f4
luigazab <luishagailcanonoy@gmail.com>
2025-08-25 21:37:51 +0800

    Cleaning

--- frontend/src/pages/Certification.jsx
+++
@@ -1,9 +0,0 @@
-export default function Certification() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold mb-4">Certification</h1>
-      <p className="text-gray-600">This page will display the certifications the solarch has accumulated.</p>
-      {/* Additional content can be added here */}
-    </div>
-  );
-}
